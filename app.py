25227d92d6f9263f25cef6c71e1198a37b7918db
Dasha <dshifrina@stuy.edu>
2017-10-04 07:17:42 -0400

    blah

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-@app.route("/")
-def home():
-    
-    return render_template("login.html")
+@app.route("/"):
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    if(username == "poo" and password == "pee"):
+        session["username"] = username
+        return redirect("/woo")
+    else:
+        return "You entered the wrong information!"
+    return render_template("form.html")
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
